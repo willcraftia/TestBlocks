81c4dbeee31cc945f15d301a1444e2e4b1b5ac1d
Will Craftia <willcraftia@gmail.com>
2012-12-05 14:40:16 +0900

    Resource loader for XML.

---
+++ Framework.Serialization.Xml/Framework.Serialization.Xml.Demo/Program.cs
@@ -0,0 +1,178 @@
+﻿#region Using
+
+using System;
+using System.IO;
+using Willcraftia.Xna.Framework.IO;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+#endregion
+
+namespace Willcraftia.Xna.Framework.Serialization.Xml.Demo
+{
+    public class Program
+    {
+        #region Demo Classes
+
+        public interface IAnimal
+        {
+            string Name { get; set; }
+        }
+
+        public class Dog : IAnimal
+        {
+            public string Name { get; set; }
+
+            public string DogProperty;
+
+            public override string ToString()
+            {
+                return "Name=" + Name + ", CatProperty=" + DogProperty;
+            }
+        }
+
+        public class Cat : IAnimal
+        {
+            public string Name { get; set; }
+
+            public string CatProperty;
+
+            public override string ToString()
+            {
+                return "Name=" + Name + ", CatProperty=" + CatProperty;
+            }
+        }
+
+        public class AnimalManager : IXmlSerializable
+        {
+            public IAnimal[] Animals;
+            
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+
+            public AnimalManager()
+            {
+                namespaces.Add(string.Empty, string.Empty);
+            }
+
+            // I/F
+            public XmlSchema GetSchema()
+            {
+                return null;
+            }
+
+            // I/F
+            public void ReadXml(XmlReader reader)
+            {
+                reader.Read();
+                reader.ReadStartElement("Animals");
+
+                var list = new System.Collections.Generic.List<IAnimal>();
+
+                while (reader.IsStartElement("Animal"))
+                {
+                    var typeString = reader.GetAttribute("Type");
+                    reader.ReadStartElement("Animal");
+
+                    var animalType = Type.GetType(typeString);
+
+                    var animalSerializer = XmlResourceLoader.Instance.GetSerializer(animalType);
+                    var animal = animalSerializer.Deserialize(reader) as IAnimal;
+
+                    list.Add(animal);
+
+                    reader.ReadEndElement();
+                }
+
+                if (list.Count != 0) Animals = list.ToArray();
+
+                reader.ReadEndElement();
+            }
+
+            // I/F
+            public void WriteXml(XmlWriter writer)
+            {
+                writer.WriteStartElement("Animals");
+
+                if (Animals == null || Animals.Length == 0) return;
+
+                foreach (var animal in Animals)
+                {
+                    writer.WriteStartElement("Animal");
+
+                    var animalType = animal.GetType();
+                    writer.WriteAttributeString("Type", animalType.FullName);
+
+                    var animalSerializer = XmlResourceLoader.Instance.GetSerializer(animalType);
+                    animalSerializer.Serialize(writer, animal, namespaces);
+
+                    writer.WriteEndElement();
+                }
+
+                writer.WriteEndElement();
+            }
+
+            public override string ToString()
+            {
+                var result = "{";
+                if (Animals != null)
+                {
+                    for (int i = 0; i < Animals.Length; i++)
+                    {
+                        result += "{";
+                        result += Animals[i];
+                        result += "}";
+                        if (i < Animals.Length - 2) result += ", ";
+                    }
+                }
+                result += "}";
+                return result.ToString();
+            }
+        }
+
+        #endregion
+
+        static void Main(string[] args)
+        {
+            //================================================================
+            // Test polymorphic objects
+
+            var animalManager = new AnimalManager();
+            animalManager.Animals = new IAnimal[]
+            {
+                new Dog { Name = "Pochi", DogProperty = "Wan wan" },
+                new Cat { Name = "Tama", CatProperty = "Nya nya" }
+            };
+
+            // Register JsonResourceLoader.
+            ResourceManager.Instance.LoaderMap[".xml"] = XmlResourceLoader.Instance;
+
+            // Output file path.
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "AnimalManager.xml");
+            var uri = new Uri(path);
+
+            // Save.
+            ResourceManager.Instance.Save(uri, animalManager);
+            Console.WriteLine("Save:");
+            Console.WriteLine(uri.LocalPath);
+
+            using (var stream = ResourceContainerManager.Instance.OpenResource(uri))
+            using (var reader = new StreamReader(stream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null) Console.WriteLine(line);
+            }
+            Console.WriteLine();
+
+            // Load.
+            var loadedObject = ResourceManager.Instance.Load<AnimalManager>(uri);
+            Console.WriteLine("Load:");
+            Console.WriteLine(loadedObject);
+            Console.WriteLine();
+
+            // Exit.
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Framework.Serialization.Xml/Framework.Serialization.Xml/XmlResourceLoader.cs
@@ -0,0 +1,85 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+#endregion
+
+namespace Willcraftia.Xna.Framework.Serialization.Xml
+{
+    public sealed class XmlResourceLoader : IResourceLoader
+    {
+        public static readonly XmlResourceLoader Instance = new XmlResourceLoader();
+
+        Dictionary<Type, XmlSerializer> cache = new Dictionary<Type, XmlSerializer>();
+        
+        public XmlWriterSettings WriterSettings { get; private set; }
+
+        public XmlReaderSettings ReaderSettings { get; private set; }
+
+        XmlResourceLoader()
+        {
+            WriterSettings = new XmlWriterSettings
+            {
+                Indent = true,
+                IndentChars = "\t",
+                Encoding = Encoding.UTF8,
+                OmitXmlDeclaration = true
+            };
+            ReaderSettings = new XmlReaderSettings
+            {
+                IgnoreComments = true,
+                IgnoreProcessingInstructions = true,
+                IgnoreWhitespace = true
+            };
+        }
+
+        public object Load(Uri uri, Stream stream, Type type)
+        {
+            var serializer = GetSerializer(type);
+
+            using (var reader = CreateXmlReader(stream))
+            {
+                return serializer.Deserialize(reader);
+            }
+        }
+
+        public void Save(Uri uri, Stream stream, object resource)
+        {
+            var serializer = GetSerializer(resource.GetType());
+
+            using (var writer = CreateXmlWriter(stream))
+            {
+                serializer.Serialize(writer, resource);
+            }
+        }
+
+        public XmlSerializer GetSerializer(Type type)
+        {
+            lock (cache)
+            {
+                XmlSerializer result;
+                if (!cache.TryGetValue(type, out result))
+                {
+                    result = new XmlSerializer(type);
+                    cache[type] = result;
+                }
+                return result;
+            }
+        }
+
+        XmlWriter CreateXmlWriter(Stream stream)
+        {
+            return XmlWriter.Create(stream, WriterSettings);
+        }
+
+        XmlReader CreateXmlReader(Stream stream)
+        {
+            return XmlReader.Create(stream, ReaderSettings);
+        }
+    }
+}
