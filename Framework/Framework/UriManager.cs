7fe9b49fe78c563a4af24dccd716888f0d151cd8
Will Craftia <willcraftia@gmail.com>
2012-12-10 00:24:41 +0900

    cache for relative uri.

--- Framework/Framework/UriManager.cs
+++ Framework/Framework/UriManager.cs
@@ -9,7 +9,61 @@ namespace Willcraftia.Xna.Framework
 {
     public sealed class UriManager
     {
+        #region RelativeUriKey
+
+        struct RelativeUriKey : IEquatable<RelativeUriKey>
+        {
+            public string BaseUri;
+
+            public string RelativeUri;
+
+            #region Equatable
+
+            public static bool operator ==(RelativeUriKey p1, RelativeUriKey p2)
+            {
+                return p1.Equals(p2);
+            }
+
+            public static bool operator !=(RelativeUriKey p1, RelativeUriKey p2)
+            {
+                return !p1.Equals(p2);
+            }
+
+            // I/F
+            public bool Equals(RelativeUriKey other)
+            {
+                return BaseUri == other.BaseUri && RelativeUri == other.RelativeUri;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (obj == null || GetType() != obj.GetType()) return false;
+
+                return Equals((RelativeUriKey) obj);
+            }
+
+            public override int GetHashCode()
+            {
+                return BaseUri.GetHashCode() ^ RelativeUri.GetHashCode();
+            }
+
+            #endregion
+
+            #region ToString
+
+            public override string ToString()
+            {
+                return "[" + BaseUri + ", " + RelativeUri + "]";
+            }
+
+            #endregion
+        }
+
+        #endregion
+
         Dictionary<string, IUri> cache = new Dictionary<string, IUri>();
+
+        Dictionary<RelativeUriKey, IUri> relativeUriCache = new Dictionary<RelativeUriKey, IUri>();
 
         public IUri Create(string uriString)
         {
@@ -26,10 +80,25 @@ namespace Willcraftia.Xna.Framework
             if (baseUri == null) throw new ArgumentNullException("baseUri");
             if (relativeUri == null) throw new ArgumentNullException("relativeUri");
 
+            IUri uri;
+            if (cache.TryGetValue(relativeUri, out uri))
+                return uri;
+
             if (0 < relativeUri.IndexOf(':'))
                 return Create(relativeUri);
 
-            return Create(baseUri + relativeUri);
+            var relativeUriKey = new RelativeUriKey
+            {
+                BaseUri = baseUri,
+                RelativeUri = relativeUri
+            };
+
+            if (relativeUriCache.TryGetValue(relativeUriKey, out uri))
+                return uri;
+
+            uri = Create(baseUri + relativeUri);
+            relativeUriCache[relativeUriKey] = uri;
+            return uri;
         }
 
         public string CreateRelativeUri(string baseUri, IUri uri)
