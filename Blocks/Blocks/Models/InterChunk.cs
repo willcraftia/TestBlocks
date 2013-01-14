44244b780b5c3148cbef9248d5b6e5acece22ab2
Will Craftia <willcraftia@gmail.com>
2013-01-15 00:19:48 +0900

    volatile

--- Blocks/Blocks/Models/InterChunk.cs
+++ Blocks/Blocks/Models/InterChunk.cs
@@ -8,11 +8,17 @@ namespace Willcraftia.Xna.Blocks.Models
 {
     public sealed class InterChunk
     {
+        volatile bool completed;
+
         public InterChunkMesh Translucent { get; private set; }
 
         public InterChunkMesh Opaque { get; private set; }
 
-        public bool Completed { get; set; }
+        public bool Completed
+        {
+            get { return completed; }
+            set { completed = value; }
+        }
 
         public InterChunk()
         {
