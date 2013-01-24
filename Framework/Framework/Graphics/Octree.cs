db9575d91a16bc247511c39279057ee15bc32c32
Will Craftia <willcraftia@gmail.com>
2013-01-24 18:11:32 +0900

    Octree -> SimpleOctree.

---
+++ Framework/Framework/Graphics/Octree.cs
@@ -0,0 +1,14 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+#endregion
+
+namespace Willcraftia.Xna.Framework.Graphics
+{
+    public sealed class Octree
+    {
+    }
+}
