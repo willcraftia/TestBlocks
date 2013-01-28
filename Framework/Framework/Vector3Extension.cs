f85c227e5cd27988cfff8cf4a4cf8cf450cc4cf7
Will Craftia <willcraftia@gmail.com>
2013-01-28 20:09:16 +0900

    Vector3Extension

---
+++ Framework/Framework/Vector3Extension.cs
@@ -0,0 +1,27 @@
+﻿#region Using
+
+using System;
+using Microsoft.Xna.Framework;
+
+#endregion
+
+namespace Willcraftia.Xna.Framework
+{
+    /// <summary>
+    /// Vector3 の拡張クラスです。
+    /// </summary>
+    public static class Vector3Extension
+    {
+        /// <summary>
+        /// ゼロ ベクトルであるか否かを検査します。
+        /// </summary>
+        /// <param name="vector">Vector3。</param>
+        /// <returns>
+        /// true (ゼロ ベクトルである場合)、false (それ以外の場合)。
+        /// </returns>
+        public static bool IsZero(this Vector3 vector)
+        {
+            return vector.X == 0 && vector.Y == 0 && vector.Z == 0;
+        }
+    }
+}
