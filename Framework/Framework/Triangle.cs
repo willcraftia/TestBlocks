b0fe748b8f048504ace801ea58b207affcd8c85f
Will Craftia <willcraftia@gmail.com>
2013-01-31 19:39:02 +0900

    Triangle. Intersection between Triangle and Ray.

---
+++ Framework/Framework/Triangle.cs
@@ -0,0 +1,105 @@
+﻿#region Using
+
+using System;
+using Microsoft.Xna.Framework;
+
+#endregion
+
+namespace Willcraftia.Xna.Framework
+{
+    public struct Triangle
+    {
+        public Vector3 V0;
+
+        public Vector3 V1;
+
+        public Vector3 V2;
+
+        public Triangle(Vector3 v0, Vector3 v1, Vector3 v2)
+        {
+            V0 = v0;
+            V1 = v1;
+            V2 = v2;
+        }
+
+        // XNA サンプル『トライアングル精度でのピッキング』より。
+        // http://xbox.create.msdn.com/ja-JP/education/catalog/sample/picking_triangle
+
+        public float? Intersects(Ray ray)
+        {
+            float? result;
+            Intersects(ref ray, out result);
+            return result;
+        }
+
+        public void Intersects(ref Ray ray, out float? result)
+        {
+            // Compute vectors along two edges of the triangle.
+            Vector3 edge1;
+            Vector3 edge2;
+
+            Vector3.Subtract(ref V2, ref V1, out edge1);
+            Vector3.Subtract(ref V0, ref V1, out edge2);
+
+            // Compute the determinant.
+            Vector3 directionCrossEdge2;
+            Vector3.Cross(ref ray.Direction, ref edge2, out directionCrossEdge2);
+
+            float determinant;
+            Vector3.Dot(ref edge1, ref directionCrossEdge2, out determinant);
+
+            // If the ray is parallel to the triangle plane, there is no collision.
+            if (determinant > -float.Epsilon && determinant < float.Epsilon)
+            {
+                result = null;
+                return;
+            }
+
+            float inverseDeterminant = 1.0f / determinant;
+
+            // Calculate the U parameter of the intersection point.
+            Vector3 distanceVector;
+            Vector3.Subtract(ref ray.Position, ref V1, out distanceVector);
+
+            float triangleU;
+            Vector3.Dot(ref distanceVector, ref directionCrossEdge2, out triangleU);
+            triangleU *= inverseDeterminant;
+
+            // Make sure it is inside the triangle.
+            if (triangleU < 0 || triangleU > 1)
+            {
+                result = null;
+                return;
+            }
+
+            // Calculate the V parameter of the intersection point.
+            Vector3 distanceCrossEdge1;
+            Vector3.Cross(ref distanceVector, ref edge1, out distanceCrossEdge1);
+
+            float triangleV;
+            Vector3.Dot(ref ray.Direction, ref distanceCrossEdge1, out triangleV);
+            triangleV *= inverseDeterminant;
+
+            // Make sure it is inside the triangle.
+            if (triangleV < 0 || triangleU + triangleV > 1)
+            {
+                result = null;
+                return;
+            }
+
+            // Compute the distance along the ray to the triangle.
+            float rayDistance;
+            Vector3.Dot(ref edge2, ref distanceCrossEdge1, out rayDistance);
+            rayDistance *= inverseDeterminant;
+
+            // Is the triangle behind the ray origin?
+            if (rayDistance < 0)
+            {
+                result = null;
+                return;
+            }
+
+            result = rayDistance;
+        }
+    }
+}
