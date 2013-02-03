c7caafc88b698cbab93f9d80ffc2b4a4665f1a74
Will Craftia <willcraftia@gmail.com>
2013-02-03 16:57:59 +0900

    modified.

--- Blocks/Blocks/Models/ChunkLightBuilder.cs
+++ Blocks/Blocks/Models/ChunkLightBuilder.cs
@@ -65,6 +65,11 @@ namespace Willcraftia.Xna.Blocks.Models
 
                     if (topNeighbor != null)
                     {
+                        // TODO
+                        //
+                        // ここで、まだ光レベルを構築していないチャンクを参照する場合があり、
+                        // そこでは全て光レベル 0 であり、自身も全て光レベル 0 となってしまう。
+
                         // 上にチャンクがあるならば、その情報を参照。
                         if (topNeighbor.GetSkylightLevel(x, 0, z) < 15)
                         {
@@ -119,6 +124,25 @@ namespace Willcraftia.Xna.Blocks.Models
             }
         }
 
+        void PropagateSkylightFromNeighbor(int x, int y, int z, byte sourceLevel)
+        {
+            const int attenuation = 1;
+
+            if (sourceLevel <= 1) return;
+
+            if (sourceLevel <= Chunk.GetSkylightLevel(x, y, z)) return;
+
+            var block = Chunk.GetBlock(x, y, z);
+            if (block == null || block.Translucent)
+            {
+                //Chunk.SetSkylightLevel(x, y, z, (byte) (sourceLevel - 1));
+
+                var value = sourceLevel - attenuation;
+                if (value < 0) value = 0;
+                Chunk.SetSkylightLevel(x, y, z, (byte) value);
+            }
+        }
+
         void PropagateSkylightFromTopNeighbor()
         {
             var neighborChunkPosition = Chunk.Position + CubicSide.Top.Direction;
@@ -130,15 +154,7 @@ namespace Willcraftia.Xna.Blocks.Models
                 for (int x = 0; x < manager.ChunkSize.X; x++)
                 {
                     var skyLight = neighborChunk.GetSkylightLevel(x, 0, z);
-                    if (skyLight <= 1) return;
-
-                    if (skyLight <= Chunk.GetSkylightLevel(x, manager.ChunkSize.Y - 1, z)) continue;
-
-                    var block = Chunk.GetBlock(x, manager.ChunkSize.Y - 1, z);
-                    if (block == null || block.Translucent)
-                    {
-                        Chunk.SetSkylightLevel(x, manager.ChunkSize.Y - 1, z, (byte) (skyLight - 1));
-                    }
+                    PropagateSkylightFromNeighbor(x, manager.ChunkSize.Y - 1, z, skyLight);
                 }
             }
         }
@@ -154,15 +170,7 @@ namespace Willcraftia.Xna.Blocks.Models
                 for (int x = 0; x < manager.ChunkSize.X; x++)
                 {
                     var skyLight = neighborChunk.GetSkylightLevel(x, manager.ChunkSize.Y - 1, z);
-                    if (skyLight <= 1) return;
-
-                    if (skyLight <= Chunk.GetSkylightLevel(x, 0, z)) continue;
-
-                    var block = Chunk.GetBlock(x, 0, z);
-                    if (block == null || block.Translucent)
-                    {
-                        Chunk.SetSkylightLevel(x, 0, z, (byte) (skyLight - 1));
-                    }
+                    PropagateSkylightFromNeighbor(x, 0, z, skyLight);
                 }
             }
         }
@@ -178,15 +186,7 @@ namespace Willcraftia.Xna.Blocks.Models
                 for (int x = 0; x < manager.ChunkSize.X; x++)
                 {
                     var skyLight = neighborChunk.GetSkylightLevel(x, y, 0);
-                    if (skyLight <= 1) return;
-
-                    if (skyLight <= Chunk.GetSkylightLevel(x, y, manager.ChunkSize.Z - 1)) continue;
-
-                    var block = Chunk.GetBlock(x, y, manager.ChunkSize.Z - 1);
-                    if (block == null || block.Translucent)
-                    {
-                        Chunk.SetSkylightLevel(x, y, manager.ChunkSize.Z - 1, (byte) (skyLight - 1));
-                    }
+                    PropagateSkylightFromNeighbor(x, y, manager.ChunkSize.Z - 1, skyLight);
                 }
             }
         }
@@ -202,15 +202,7 @@ namespace Willcraftia.Xna.Blocks.Models
                 for (int x = 0; x < manager.ChunkSize.X; x++)
                 {
                     var skyLight = neighborChunk.GetSkylightLevel(x, y, manager.ChunkSize.Z - 1);
-                    if (skyLight <= 1) return;
-
-                    if (skyLight <= Chunk.GetSkylightLevel(x, y, 0)) continue;
-
-                    var block = Chunk.GetBlock(x, y, 0);
-                    if (block == null || block.Translucent)
-                    {
-                        Chunk.SetSkylightLevel(x, y, 0, (byte) (skyLight - 1));
-                    }
+                    PropagateSkylightFromNeighbor(x, y, 0, skyLight);
                 }
             }
         }
@@ -226,15 +218,7 @@ namespace Willcraftia.Xna.Blocks.Models
                 for (int y = 0; y < manager.ChunkSize.Y; y++)
                 {
                     var skyLight = neighborChunk.GetSkylightLevel(manager.ChunkSize.X - 1, y, z);
-                    if (skyLight <= 1) return;
-
-                    if (skyLight <= Chunk.GetSkylightLevel(0, y, z)) continue;
-
-                    var block = Chunk.GetBlock(0, y, z);
-                    if (block == null || block.Translucent)
-                    {
-                        Chunk.SetSkylightLevel(0, y, z, (byte) (skyLight - 1));
-                    }
+                    PropagateSkylightFromNeighbor(0, y, z, skyLight);
                 }
             }
         }
@@ -250,15 +234,7 @@ namespace Willcraftia.Xna.Blocks.Models
                 for (int y = 0; y < manager.ChunkSize.Y; y++)
                 {
                     var skyLight = neighborChunk.GetSkylightLevel(0, y, z);
-                    if (skyLight <= 1) return;
-
-                    if (skyLight <= Chunk.GetSkylightLevel(manager.ChunkSize.X - 1, y, z)) continue;
-
-                    var block = Chunk.GetBlock(manager.ChunkSize.X - 1, y, z);
-                    if (block == null || block.Translucent)
-                    {
-                        Chunk.SetSkylightLevel(manager.ChunkSize.X - 1, y, z, (byte) (skyLight - 1));
-                    }
+                    PropagateSkylightFromNeighbor(manager.ChunkSize.X - 1, y, z, skyLight);
                 }
             }
         }
